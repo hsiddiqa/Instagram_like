e2c0797b4a8272a3c2a33a0e6546412fcc10a2b1
hsiddiqa <humaira.siddiqa@gmail.com>
2019-10-27 06:11:28 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -27,15 +27,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   assert_not flash.empty?
   assert_redirected_to login_url
   end
-  
-  # test "should not allow the admin attribute to be edited via the web" do
-  #   log_in_as(@other_user)
-  #   assert_not @other_user.admin?
-  #   patch user_path(@other_user), params: {user: { password:FILL_IN,
-  #                                               password_confirmation: FILL_IN,
-  #                                               admin: FILL_IN } }
-  #   assert_not @other_user.FILL_IN.admin?
-  # end
+
   
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
@@ -66,5 +58,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
-  
+    test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
